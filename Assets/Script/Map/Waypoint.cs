8af046ae5dc5aaaee0e0c2ea130f8d6d66115fea
matt.langley <matt.tarquin@gmail.com>
2019-10-12 00:29:25 +0100

    added waypoints.

---
+++ Assets/Script/Map/Waypoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Waypoint : MonoBehaviour
+{
+    public float weight;
+    public Waypoint[] linked;
+
+    public abstract void GetNext();
+}
+
